71a85e6a065c9718509454104fe8da52299cb7e2
SuperGomoo <mike2827@hanyang.ac.kr>
2023-01-12 01:32:09 +0900

    Added new logic - album selection

--- music_search_engine/src/music_search_engine_ver01/Main.java
+++ music_search_engine/src/music_search_engine_ver01/Main.java
@@ -7,11 +7,43 @@ public class Main {
 	public static void main(String[] args) {
 		Engine_ver01 eng = new Engine_ver01();
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("드림 씨어터의 노래 이름을 한글 또는 영어로 입력하세요. (대소문자, 띄어쓰기는 무시해도 됨.)");
-		String song_name = eng.setName(sc.nextLine());
+		String answer;
 		
-		eng.info(song_name);
+		 try {
+	            System.out.println("Welcome to music search engine!");
+	            Thread.sleep(2000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+
+		 System.out.println("Do you know the name of the song you're looking for? (y / n)");
+		 
+		 answer = sc.nextLine();
+		 
+		 if (answer.equals("y")) {
+			System.out.println("Type a name of a song of Dream Theater. (Ignore upper/lower cases and spacing.)");
+			String song_name = eng.setName(sc.nextLine());
+			eng.info(song_name);
+			
+		 } else {
+			System.out.println("Type a number of the album that the song belongs to.\n"
+		                           + "1 : 1st\n"
+		                           + "2 : 2nd\n"
+		                           + "3 : 3rd\n"
+		                           + "4 : 4th");
+			int album = eng.setAlbum(sc.nextInt()); 
+		 }
+		 
+//		System.out.println("Type a name of a song of Dream Theater. (Ignore upper/lower cases and spacing.)\n"
+//				               + "Or if you don't remember the name, type a number of the album that the song belongs to.\n"
+//				               + "1 : 1st\n"
+//				               + "2 : 2nd\n"
+//				               + "3 : 3rd\n"
+//				               + "4 : 4th\n");
+//		
+//		String song_name = eng.setName(sc.nextLine());
+		
+//		eng.info(song_name);
 		
 		sc.close();
 	}
